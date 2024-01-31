e1e44860ded82e93d5747f3e3acb13a7636c76aa
Renata Pereira <renatarpsm@gmail.com>
2024-01-31 12:28:55 -0300

    crud.produto

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,89 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O nome é obrigatorio!")
+	@Size(min = 2, max = 100, message = "O nome deve conter no mínimo 05 e no maximo 100 caracteres")
+	private String nome;
+
+	@NotNull(message = "O preço é obrigatorio!")
+	private Double preco;
+
+	@NotNull(message = "O quantidade é obrigatorio!")
+	private Integer quantidade;
+
+	@NotBlank(message = "O fabricante é obrigatório")
+	@Size(min = 10, max = 100, message = "O fabricante deve ter no mínimo 10 e no máximo 100 caracteres")
+	private String fabricante;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
